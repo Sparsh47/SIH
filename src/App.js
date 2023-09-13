23ae389253cb293cdc0e9a550be2421a6080997a
Pritish4113 <96116121+Pritish4113@users.noreply.github.com>
2023-09-13 22:55:39 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 import LandingPage from "./components/LandingPage";
 import HomePage from './components/HomePage';
@@ -6,30 +5,10 @@ import HomePage from './components/HomePage';
 
 function App() {
   return (
-    <div style={{backgroundColor : "#4f8098"}} w="100%">
+    <div style={{backgroundColor : "#4f8098"}} w="100%" className='hi'>
       <Route exact path="/" component={HomePage}></Route>
       <Route exact path="/landing" component={LandingPage}></Route>
     </div>
-=======
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import CoursesSwiper from "./components/CoursesSwiper";
-import Footer from "./components/Footer";
-import { Box } from "@chakra-ui/react";
-
-const generalStyles = {
-  "font-family": "'Poppins', sans-serif",
-};
-
-function App() {
-  return (
-    <Box sx={generalStyles}>
-      <Navbar />
-      <Hero />
-      <CoursesSwiper />
-      <Footer />
-    </Box>
->>>>>>> 52e0ce14e7c123017dc1aba9e9762f2c3fa40192
   );
 }
 
