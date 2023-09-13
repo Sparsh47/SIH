52e0ce14e7c123017dc1aba9e9762f2c3fa40192
Sparsh Shandilya <sparshshandilya123@gmail.com>
2023-09-13 20:30:05 +0530

    Footer Completed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import CoursesSwiper from "./components/CoursesSwiper";
 import Footer from "./components/Footer";
+import { Box } from "@chakra-ui/react";
+
+const generalStyles = {
+  "font-family": "'Poppins', sans-serif",
+};
 
 function App() {
   return (
-    <div w="100%">
+    <Box sx={generalStyles}>
       <Navbar />
       <Hero />
       <CoursesSwiper />
       <Footer />
-    </div>
+    </Box>
   );
 }
 
