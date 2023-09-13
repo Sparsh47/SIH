7fb01b93159434067fdab30ddd2f8d541c5f90f4
Sparsh Shandilya <sparshshandilya123@gmail.com>
2023-09-13 12:20:56 +0530

    Basic React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Heading, Text } from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <Heading>Hello World</Heading>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam,
+        justo sed cursus volutpat, ex odio vulputate eros, sed hendrerit arcu
+        purus eu orci. Integer ac justo id justo mattis convallis. Ut venenatis
+        in lectus ac tincidunt.
+      </Text>
+    </Container>
   );
 }
 
